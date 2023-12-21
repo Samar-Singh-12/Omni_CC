b06e7c556d612cbcca2ca71b343222bc388d7bda
Antariksh Singh <antarikshsingh212@gmail.com>
2023-12-21 19:45:14 +0530

    updated bat file

--- pre_requisite.bat
+++ pre_requisite.bat
@@ -1,7 +1,11 @@
 python.exe -m pip install --upgrade pip
 pip install pyinputplus
+pip install --upgrade pyinputplus
 pip install python-dateutil
+pip install --upgrade python-dateutil
 pip install requests
+pip install --upgrade requests
 pip install openpyxl
+pip install --upgrade openpyxl
 pip install certifi
 pip install --upgrade certifi
